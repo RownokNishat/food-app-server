7d60681ef7fe82c463af1490bcac4c7ac9f57306
RownokNishat <rownokjahan900@gmail.com>
2023-03-06 01:39:00 +0600

    update category and food for admin

--- index.js
+++ index.js
@@ -18,23 +18,25 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    const usersCollection = client.db("tastyFood").collection("users");
     const foodCollection = client.db("tastyFood").collection("foodService");
     const orderCollection = client.db("tastyFood").collection("orders");
     const catagoriesCollection = client
       .db("tastyFood")
       .collection("catagories");
-    app.get("/catagories", async (req, res) => {
-      const query = {};
-      const cursor = catagoriesCollection.find(query);
-      const foodService = await cursor.toArray();
-      res.send(foodService);
-    });
+    const foodcategoryCollection = client
+      .db("tastyFood")
+      .collection("foodCategory");
 
-    app.get("/details/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const foodService = await catagoriesCollection.findOne(query);
-      res.send(foodService);
+    const foodDetailsCollection = client
+      .db("tastyFood")
+      .collection("foodDetails");
+
+    //////////////users get and post//////////
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
     });
 
     ////////orders api///////
@@ -45,16 +47,86 @@ async function run() {
     });
 
     app.get("/orders", async (req, res) => {
-      const query = {};
+      let query = {};
+      if (req.query.email) {
+        query = { email: req.query.email };
+      }
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
       res.send(orders);
     });
 
-    app.post("/catagories", async (req, res) => {
-      const catagories = req.body;
-      const result = await catagoriesCollection.insertOne(catagories);
+    ///////////////////hotel////////////////////////
+
+    app.post("/admin/AddFoodCategory", async (req, res) => {
+      console.log(req.body);
+      const category = req.body;
+      const cursor = foodcategoryCollection.find({
+        categoryName: req.body.categoryName,
+      });
+      const categories = await cursor.toArray();
+      if (categories.length == 0) {
+        const result = await foodcategoryCollection.insertOne(category);
+        res.send(result);
+      } else {
+        res.send({ message: "already in db" });
+      }
+    });
+
+    ///////category///// jeikhane admin food add krbe oikhane category select kore dibe noyto category wise  data khuje pabo na
+    app.get("/admin/getCatagories", async (req, res) => {
+      const query = {};
+      const cursor = foodcategoryCollection.find(query);
+      const foodService = await cursor.toArray();
+      res.send(foodService);
+    });
+
+    app.post("/admin/addFood", async (req, res) => {
+      const result = await foodDetailsCollection.insertOne(req.body);
       res.send(result);
+    });
+
+    ////////////get all fooods ///////
+    app.get("/getAllFoods", async (req, res) => {
+      const query = {};
+      const cursor = foodDetailsCollection.find(query);
+      const foodService = await cursor.toArray();
+      res.send(foodService);
+    });
+
+    ///// 1. getAllFoods
+    app.put("/admin/foodUpdate/:id", async (req, res) => {
+      const query = { _id: req.params.id };
+      const updaeData = req.body;
+      console.log(query, updaeData);
+      const cursor = await foodDetailsCollection.findOneAndUpdate(
+        { _id: new ObjectId(req.params.id) },
+        {
+          $set: {
+            foodName: req.body.foodName,
+            price: req.body.price,
+            details: req.body.details,
+            imageUrl: req.body.imageUrl,
+            categoryName: req.body.categoryName,
+          },
+        }
+      );
+      // const foodService = await cursor.toArray();
+      res.send(cursor);
+    });
+
+    app.delete("/admin/foodDelete/:id", async (req, res) => {
+      const cursor = await foodDetailsCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(cursor);
+    });
+
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const foodService = await catagoriesCollection.findOne(query);
+      res.send(foodService);
     });
   } finally {
   }
